ff746c33e062bab4b07c5fbaec25e4a269012c35
Prakash Toppo <toppoprakash123@gmail.com>
2026-02-14 23:38:30 +0530

    initialize Spring Boot project with postgresql and docker support

---
+++ src/main/java/com/example/virtualCard/VirtualCardApplication.java
@@ -0,0 +1,13 @@
+package com.example.virtualCard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualCardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualCardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/virtualCard/VirtualCardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.virtualCard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualCardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
